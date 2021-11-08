f45616588844724dcecdaf6299b1fc6ec2d78644
DESKTOP-F2TNL45\aquil <apinao2000@alumno.ipn.mx>
2021-11-08 12:07:19 -0600

    pide datos al usuario

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,9 +6,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int cont,can;
+            string nombre,grup,conv;
 
+            Console.WriteLine("Bienvenido");//Mensaje de bienvenida 
+            /*El programa para imprimir una discografia */
+            Console.WriteLine("Intoduce tu nombre ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Digita el album  del grupo");
+            grup = Console.ReadLine();
+            Console.WriteLine("Cuantas canciones contiene el album");
+            conv = Console.ReadLine();
+            can = int.Parse(conv);
+            string[] discografia = new string[can];
+            
+            for (cont = 0; cont < can; cont++)
+            {
+         
+                Console.WriteLine("Introduzca la cancion :",cont);
+                discografia[cont] = Console.ReadLine();
+            }
 
+            Console.WriteLine(grup);
+             for (cont = 0; cont <= can; cont++)
+            {
+               Console.WriteLine(discografia[can]);
+            }
         }
     }
 }
