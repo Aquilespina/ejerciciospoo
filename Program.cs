44f111daa367c0b9587ed291a5982340fbce1b6c
DESKTOP-F2TNL45\aquil <apinao2000@alumno.ipn.mx>
2021-11-08 17:12:58 -0600

    Registrar datos pero sin imprimir

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace registrar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            string[] discografia = { };
+            int cont, disco, dis;
+            string nom, x, art;
+            Console.WriteLine("Digite tu nombre ");
+            nom = Console.ReadLine();
+            Console.WriteLine("Bienveid@ " + nom);
+            Console.WriteLine("Digite el artista ");
+            art = Console.ReadLine();
+            Console.WriteLine("Digite el numero de canciones");
+            x= Console.ReadLine();
+            dis = int.Parse(x);
+            for (cont = 0; cont < dis; cont++)
+            {
+                Console.WriteLine("Introduzca la cancion ");
+                discografia[cont]=Console.ReadLine();
+               
+            }
+            for (cont = 0; cont < dis; cont++)
+            {
+                Console.WriteLine("La cancion que contiene es :");
+                Console.WriteLine(discografia[cont]);
+            }
+            
         }
     }
 }
