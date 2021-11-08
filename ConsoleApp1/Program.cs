6807b043d45824023d9611b6e944c5e9ccefc663
DESKTOP-F2TNL45\aquil <apinao2000@alumno.ipn.mx>
2021-11-08 12:50:41 -0600

    Imprime discografia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,33 +6,40 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
-            int cont,can;
-            string nombre,grup,conv;
-
-            Console.WriteLine("Bienvenido");//Mensaje de bienvenida 
-            /*El programa para imprimir una discografia */
-            Console.WriteLine("Intoduce tu nombre ");
-            nombre = Console.ReadLine();
-            Console.WriteLine("Digita el album  del grupo");
-            grup = Console.ReadLine();
-            Console.WriteLine("Cuantas canciones contiene el album");
-            conv = Console.ReadLine();
-            can = int.Parse(conv);
-            string[] discografia = new string[can];
-            
-            for (cont = 0; cont < can; cont++)
+            /*El programa es para imprimir datos al usuario*/
+            int cont, aux,disco,aux2;
+            string dis;
+            string[]discografia = {"Sin miedo","Love paraiso","Todo rico" };
+            string[] discografia2 = { "Remote","Outsidein","HYBRID" };
+            string nom, dato;
+            Console.WriteLine(" Digite su nombre ");
+            nom = Console.ReadLine();
+            Console.WriteLine("Bienvenid@" + nom);
+            Console.WriteLine("Seleccione la discografia");
+            Console.WriteLine("[1]Discografia de la  garfield");
+            Console.WriteLine("[2]Discografia de  swingrowers");
+            dis = Console.ReadLine();
+            disco = int.Parse(dis);
+            aux = discografia.Length;
+            aux2 = discografia2.Length;
+            switch (disco)
             {
-         
-                Console.WriteLine("Introduzca la cancion :",cont);
-                discografia[cont] = Console.ReadLine();
+                case 1:
+                    Console.WriteLine("La discografia es :");
+                    for (cont=0;cont<aux;cont++)
+                    {
+                        Console.WriteLine(discografia[cont]);
+                    }
+                break;
+                case 2:
+                    Console.WriteLine("La discografia es :");
+                    for (cont = 0; cont < aux2; cont++)
+                    {
+                        Console.WriteLine(discografia2[cont]);
+                    }
+                    break;
             }
-
-            Console.WriteLine(grup);
-             for (cont = 0; cont <= can; cont++)
-            {
-               Console.WriteLine(discografia[can]);
-            }
+            
         }
     }
 }
