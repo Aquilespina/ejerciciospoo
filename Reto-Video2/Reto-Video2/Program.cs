cf176dc127c8896a70e36a874f67826198f21267
DESKTOP-F2TNL45\aquil <apinao2000@alumno.ipn.mx>
2021-11-10 09:51:17 -0600

    Programas del video 2 del curso

---
+++ Reto-Video2/Reto-Video2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reto_Video2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al curso de c#");
+        }
+    }
+}
