7db07c42252f87caaa58c53da7f2dba46141b3d1
DESKTOP-F2TNL45\aquil <apinao2000@alumno.ipn.mx>
2021-11-10 10:19:41 -0600

    Reto del video 4

---
+++ Reto-video4/Reto-video4/Reto-video4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Reto_video4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //.WriteLine("Hello World!");
+            int edad;/*Declaracion e iniciacion */
+            edad = 28;
+            Console.WriteLine(edad);
+        }
+    }
+}
