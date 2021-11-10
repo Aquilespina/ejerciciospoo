d1b1826f53ccdeeb57368ddf33d170f2ec1276dd
DESKTOP-F2TNL45\aquil <apinao2000@alumno.ipn.mx>
2021-11-10 15:12:52 -0600

    Video5 del curso c#

---
+++ Video5/Video5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Video5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 19;
+            // Console.WriteLine("Tienes una edad de "+edad + "años");
+            //Console.WriteLine($"Tienes una edad de {edad}");//Inter polacion de string
+            edad += 8;
+            Console.WriteLine("Tienes una edad de " + edad + "años");
+        }
+    }
+}
